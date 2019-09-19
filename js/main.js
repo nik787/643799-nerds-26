44a308fcfcbb2af09b1ad28ffe6e67467a7fca56
Nikita <43641576+nik787@users.noreply.github.com>
2019-09-19 13:59:50 +0300

    Добавил стили для шапки и слайдера

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
+var slides = document.querySelectorAll(".slider__list .slide");
+var slidesDot = document.querySelectorAll(".slider__navigation .dot");
+
+
+
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,5000);
+
+function nextSlide(){
+    slides[currentSlide].classList.remove('active');
+    slidesDot[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].classList.add('active');
+    slidesDot[currentSlide].classList.add('active');
+}
+
+
+
 ymaps.ready(function () {
   var myMap = new ymaps.Map('map', {
           center: [59.938631, 30.323055],
           zoom: 17,
           controls: [],
-          type: 'yandex#map'
+          type: 'yandex#map',
+
       }, {
           searchControlProvider: 'yandex#search'
       },
@@ -31,7 +50,6 @@ ymaps.ready(function () {
           // её "ножки" (точки привязки).
           iconImageOffset: [-50, -200]
       })
-
   myMap.geoObjects
       .add(myPlacemark)
 });
